809a15dbf926a6a98dce2e97fc49645d8fd9b759
IGNNE <30634883+IGNNE@users.noreply.github.com>
2021-03-02 10:30:33 +0100

    WIP of major rewrite

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 
 /**
  * @file ESC and servo controller based on pololu BabyOrangutan 328
- * 
+ *
  * Hardware setup: TODO
- * 
+ *
  * TODO:
  * - servos
  * - i2c slave
@@ -32,22 +32,35 @@
 #define SERVO_3_PIN PD2
 Servo servo1;
 
-#define I2C_ADDRESS 4
-#define I2C_NO_REQUEST 0xFF
+#define I2C_ADDRESS 0x04
+#define I2C_NUM_REGISTERS 6
+// permissions
+#define I2C_R 1
+#define I2C_W 2
+#define I2C_RW (I2C_R & I2C_W)
 // return values
 #define I2C_OK 1
 #define I2C_NOK 0
 // commands
+#define I2C_CHIP_ID 0
 #define I2C_CMD_BATTERY 1
 #define I2C_CMD_SERVO1 2
 #define I2C_CMD_SERVO2 3
 #define I2C_CMD_SERVO3 4
 #define I2C_CMD_ESC 5
+
+/// "registers" for the i2c client
+volatile uint16_t i2c_register[I2C_NUM_REGISTERS];
+/// permissions for the registers
+const uint8_t i2c_register_permissions[I2C_NUM_REGISTERS] = { I2C_R, I2C_R, I2C_RW, I2C_RW, I2C_RW, I2C_RW };
+
+#define I2C_NO_REQUEST 0 // TODO FIXME: 
+
 /**
  * I2C should communicate as follows:
  * 1. Master sends one byte command
- * 2.a Slave responds with a '1' for OK, '0' for not OK
- * 2.b Slave sends the requested info byte
+ * 2.a Master requests data ad index ::request_byte
+ * 2.b Master writes data to index ::request_byte
  */
 volatile uint8_t request_byte = I2C_NO_REQUEST;
 
@@ -55,93 +68,104 @@ volatile uint8_t request_byte = I2C_NO_REQUEST;
  * Sleep in increments of 10 ms, because _delay_ms can sleep at max 13 ms
  */
 void long_delay(uint8_t ten_ms) {
-  for(uint8_t i = 0; i <= ten_ms; i++) {
-    _delay_ms(10);
-  }
+    for(uint8_t i = 0; i <= ten_ms; i++) {
+        _delay_ms(10);
+    }
 }
 
 uint8_t get_battery_voltage() {
-  return uint8_t(ADCH);
+    return uint8_t(ADCH);
 }
 
 void M1_forward(unsigned char pwm)
 {
-  OCR0A = 0;
-  OCR0B = pwm;
+    OCR0A = 0;
+    OCR0B = pwm;
 }
 
 void M1_reverse(unsigned char pwm)
 {
-  OCR0B = 0;
-  OCR0A = pwm;
+    OCR0B = 0;
+    OCR0A = pwm;
 }
 
 void M2_forward(unsigned char pwm)
 {
-  OCR2A = 0;
-  OCR2B = pwm;
+    OCR2A = 0;
+    OCR2B = pwm;
 }
 
 void M2_reverse(unsigned char pwm)
 {
-  OCR2B = 0;
-  OCR2A = pwm;
+    OCR2B = 0;
+    OCR2A = pwm;
 }
 
 void i2c_on_request() {
-  switch (request_byte)
-  {
+    switch (request_byte)
+    {
     case I2C_CMD_BATTERY:
-      Wire.write(get_battery_voltage());
-      break;
+        Wire.write(get_battery_voltage());
+        break;
 
     case I2C_NO_REQUEST:
-      // something went wrong, answer nok
-      Wire.write(I2C_NOK);
-      break;
+        // something went wrong, answer nok
+        Wire.write(I2C_NOK);
+        break;
     default:
-      // command already done, just answer ok
-      Wire.write(I2C_OK);
-      break;
-  } 
-  // toggle led to show that something is working
-  PORTD ^= _BV(ULED);
+        // command already done, just answer ok
+        Wire.write(I2C_OK);
+        break;
+    }
+    // toggle led to show that something is working
+    PORTD ^= _BV(ULED);
 }
 
 void i2c_on_receive(int num_of_bytes) {
-  // TODO: check 3-byte servo commands
-  if(num_of_bytes == 3) {
 
-    // 3 byte set command, for servo timings
+    // new transaction starts here
     request_byte = Wire.read();
-    uint16_t payload = Wire.read() + (Wire.read() << 16);
-    switch (request_byte)
-    {
-      case I2C_CMD_SERVO1:
-        servo1.writeMicroseconds(payload);
-        break;
-      case I2C_CMD_ESC:
-        M1_forward(payload/4096);
-        break;
-      default:
-        // command not understood
-        request_byte = I2C_NO_REQUEST;
-        break;
+    if(request_byte < I2C_NUM_REGISTERS) {
+        // TODO: Finish this
+        // TODO: check 3-byte servo commands
+        if(num_of_bytes == 3) {
+
+            // 3 byte set command, for servo timings
+            request_byte = Wire.read();
+            uint16_t payload = Wire.read() + (Wire.read() << 16);
+            switch (request_byte) {
+            case I2C_CMD_SERVO1:
+                servo1.writeMicroseconds(payload);
+                break;
+            case I2C_CMD_ESC:
+                M1_forward(payload/4096);
+                break;
+            default:
+                // command not understood
+                request_byte = I2C_NO_REQUEST;
+                break;
+            }
+
+        } else if(num_of_bytes == 1) {
+
+            // 1 byte get command, pass on to request callback
+            request_byte = Wire.read();
+
+        } else {
+            // something went wrong
+            // empty buffer
+            while(Wire.available()) {
+                Wire.read();
+            }
+            request_byte = I2C_NO_REQUEST;
+        }
     }
 
-  } else if(num_of_bytes == 1) {
 
-    // 1 byte get command, pass on to request callback
-    request_byte = Wire.read();
 
-  } else {
-    // something went wrong
-    // empty buffer
-    while(Wire.available()) { Wire.read(); }
-    request_byte = I2C_NO_REQUEST;
-  }
-  // toggle led to show that something is working
-  PORTD ^= _BV(ULED);
+
+    // toggle led to show that something is working
+    PORTD ^= _BV(ULED);
 }
 
 
@@ -149,63 +173,67 @@ void i2c_on_receive(int num_of_bytes) {
 //  before you use any of the above functions
 void motors_init()
 {
-	// configure for inverted PWM output on motor control pins:
-	//  set OCxx on compare match, clear on timer overflow
-	//  Timer0 and Timer2 count up from 0 to 255
-	TCCR0A = TCCR2A = 0xF3;
+    // configure for inverted PWM output on motor control pins:
+    //  set OCxx on compare match, clear on timer overflow
+    //  Timer0 and Timer2 count up from 0 to 255
+    TCCR0A = TCCR2A = 0xF3;
 
-	// use the system clock/8 (=2.5 MHz) as the timer clock
-	TCCR0B = TCCR2B = 0x02;
+    // use the system clock/8 (=2.5 MHz) as the timer clock
+    TCCR0B = TCCR2B = 0x02;
 
-	// initialize all PWMs to 0% duty cycle (braking)
-	OCR0A = OCR0B = OCR2A = OCR2B = 0;
+    // initialize all PWMs to 0% duty cycle (braking)
+    OCR0A = OCR0B = OCR2A = OCR2B = 0;
 
-	// set PWM pins as digital outputs (the PWM signals will not
-	// appear on the lines if they are digital inputs)
-	DDRD |= (1 << PORTD3) | (1 << PORTD5) | (1 << PORTD6);
-	DDRB |= (1 << PORTB3);
+    // set PWM pins as digital outputs (the PWM signals will not
+    // appear on the lines if they are digital inputs)
+    DDRD |= (1 << PORTD3) | (1 << PORTD5) | (1 << PORTD6);
+    DDRB |= (1 << PORTB3);
 }
 
 void setup() {
 
-  // setup motors
-  motors_init();
+    // setup motors
+    motors_init();
 
-  // setup servos
-  servo1.attach(SERVO_1_PIN);
+    // setup servos
+    servo1.attach(SERVO_1_PIN);
 
-  // setup i2c slave
-  Wire.begin(I2C_ADDRESS);
-  // just to be safe 
-  digitalWrite(SDA, 0);
-  digitalWrite(SCL, 0);
-  Wire.onRequest(i2c_on_request);
-  Wire.onReceive(i2c_on_receive);
+    // setup i2c slave
+    Wire.begin(I2C_ADDRESS);
+    // just to be safe
+    digitalWrite(SDA, 0);
+    digitalWrite(SCL, 0);
+    Wire.onRequest(i2c_on_request);
+    Wire.onReceive(i2c_on_receive);
 
-  // setup battery adc
-  // free running, no ints, max. prescaler
-  ADCSRA = 0b10100111;
-  // channel 1, external reference, 8 bit only
-  ADMUX = 0b00100001;
-  // start ad
-  ADCSRA |= _BV(ADSC);
+    // setup battery adc
+    // free running, no ints, max. prescaler
+    ADCSRA = 0b10100111;
+    // channel 1, external reference, 8 bit only
+    ADMUX = 0b00100001;
+    // start ad
+    ADCSRA |= _BV(ADSC);
 
 
 
-  // setup led and blink to tell that we are ready
-  DDRD |= _BV(ULED);
-  for(uint8_t i = 0; i < 5; i++) {
-    PORTD ^= _BV(ULED);
-    long_delay(50);
-  }
+    // setup led and blink to tell that we are ready
+    DDRD |= _BV(ULED);
+    for(uint8_t i = 0; i < 5; i++) {
+        PORTD ^= _BV(ULED);
+        long_delay(50);
+    }
 
-  // test-run motor
-  M1_forward(64);
-  _delay_ms(10);
-  M1_forward(0);
+    // test-run motor
+    M1_forward(65);
+    _delay_ms(10);
+    M1_forward(0);
+    _delay_ms(200);
+    M1_forward(65);
+    _delay_ms(10);
+    M1_forward(0);
 }
 
 void loop() {
-  
+
 
 }
